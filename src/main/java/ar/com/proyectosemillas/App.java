96f981d35a2cc8fc62a3ce4eb18a7f10bbbe6da5
Sol <noguerasol22@gmail.com>
2021-07-20 12:04:06 -0300

    First Commit

---
+++ src/main/java/ar/com/proyectosemillas/App.java
@@ -0,0 +1,13 @@
+package ar.com.proyectosemillas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
